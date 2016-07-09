ea05afb7c0b9633ea3fe37bda7b79168c2249ce2
jarlehansen <jarle@jarlehansen.net>
2016-07-09 17:16:49 +0200

    updated

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ rm -rf .git
 npm install
 projectName="${PWD##*/}"
 sed -i '' -e "s/spectacle-boilerplate/$projectName/g" package.json
+sed -i '' -e "s/spectacle-boilerplate/$projectName/g" src/index.tmpl.html
 echo "# $projectName" > README.md
 rm -- "$0"
